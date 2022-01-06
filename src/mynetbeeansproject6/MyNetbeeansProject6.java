dd67581f33e36c9c1fc2ffe38f71efb7c7aa6790
PetersonNormil <petersonnormil@gmail.com>
2022-01-06 12:53:11 -0500

    commit 2
    my name is pete commit

--- src/mynetbeeansproject6/MyNetbeeansProject6.java
+++ src/mynetbeeansproject6/MyNetbeeansProject6.java
@@ -17,6 +17,7 @@ public class MyNetbeeansProject6 {
     public static void main(String[] args) {
         // TODO code application logic here
         //here is peterson code, to make a pull request
+        //My name is peterson
     }
     
 }
